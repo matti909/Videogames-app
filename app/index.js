bca242aad32ca7d1603ed27bca989b8893bfae06
matti909 <matias.saantiago@gmail.com>
2023-03-06 15:48:36 -0300

    ent

--- app/index.js
+++ app/index.js
@@ -1,14 +1,13 @@
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
-const getGenres = require('../app/src/controllers/getGenres.js');
+//const getGenres = require('../app/src/controllers/getGenres.js');
 
 require('dotenv').config();
 const {PORT} = process.env;
 
-//getApiInfoGenres()
 
 conn.sync({ force: true }).then(() => {
   server.listen(PORT, () => {
     console.log('%s listening at', PORT);
   });
-}).then(getGenres());
+})
--- app/src/routes/genres.router.js
+++ app/src/routes/genres.router.js
@@ -1,9 +1,11 @@
 var express = require("express");
 var router = express.Router();
 const { Genre } = require("../db");
+const getGenres = require("../controllers/getGenres");
 
 router.get("/", async (_, res) => {
   try {
+    const all = getGenres();
     const genres = await Genre.findAll();
     res.json(genres);
   } catch (e) {
